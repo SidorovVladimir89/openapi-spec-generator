2ce8277662bb5f7e8816acc52da015c43944b3b1
Vladimir Sidorov <sidorov.vladimir.89@inbox.ru>
2022-02-03 13:49:37 +0300

    fix: save to local

--- src/OpenApiGenerator.php
+++ src/OpenApiGenerator.php
@@ -25,7 +25,8 @@ class OpenApiGenerator
             Storage::put($serverKey.'_openapi.yaml', $output);
           } elseif ($format === 'json') {
             $output = json_encode($openapi->toArray(), JSON_PRETTY_PRINT);
-            Storage::put($serverKey.'_openapi.json', $output);
+            /** TODO: добавить возможность настраивать драйвер и место сохранения */
+            \Illuminate\Support\Facades\Storage::disk('local')->put($serverKey . '_openapi.json', $output);
           }
 
           return $output;
